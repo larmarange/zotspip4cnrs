af6d34da4cd140b12ec1cd0195f46e7a8fbaac15
Joseph <joseph@larmarange.net>
2014-12-14 17:51:32 +0200

    Compatibilité ZotSpip

---
+++ lang/paquet-zotspip4cnsr_fr.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// Z
+	'zotspip4cnrs_description' => 'Ce plugin fournit les squelettes permettant d\'utiliser les plugins ZotSpip, BlioCheck et Bibliographie AERES avec le kit CNRS.',
+	'zotspip4cnrs_slogan' => 'ZotSpip & co pour le kit CNRS'
+);
+
+?>
